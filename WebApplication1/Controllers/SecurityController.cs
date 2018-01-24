5656ffd01ec975027dda29003a402161e177c442
darren <764190362@qq.com>
2018-01-24 09:00:22 +0800

    产生token

---
+++ WebApplication1/Controllers/SecurityController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using WebApplication1.Models;
+using System.Security.Claims;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using System.IdentityModel.Tokens.Jwt;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication1.Controllers
+{
+   
+    public class SecurityController : Controller
+    {
+        private IOptions<ApplicationSettings> _settings;
+        public SecurityController(IOptions<ApplicationSettings> settings)
+        {
+            _settings = settings;
+        }
+       public IActionResult RequestToken([FromBody]TokenRequest request)
+        {
+            if(request.Username == "darren" && request.Password == "888888")
+            {
+                var claims = new[]
+                {
+                    new Claim(ClaimTypes.Name, request.Username)
+                };
+
+                var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_settings.Value.SecurityKey));
+                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+                var token = new JwtSecurityToken(
+                    issuer: "domain.com",
+                    audience:"domain.com",
+                    claims: claims,
+                    expires:DateTime.Now.AddMinutes(30),
+                    signingCredentials: creds);
+
+                return Ok(new {
+                    token = new JwtSecurityTokenHandler().WriteToken(token)
+                });
+            }
+            return BadRequest("couldn't verify username and password");
+            
+        }
+    }
+}
---
+++ WebApplication1/Models/ApplicationSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class ApplicationSettings
+    {
+        //需要借助 IOptions，这里才有值
+        public string TestSetting { get; set; }
+        public string SecurityKey { get; set; }
+    }
+}
